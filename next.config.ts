bc647dfc0703e5010b5e50cceb8b726d9f9ecdc8
Atulbedwal <atulbedwal69@gmail.com>
2024-12-08 02:00:15 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/',
-        destination: '/app/dashboard',
+        destination: '/dashboard',
         permanent: true, 
       },
     ];
