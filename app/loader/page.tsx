76d454c749f6b5877fed17974fb1d0cd6c5b5f9c
Atulbedwal <atulbedwal69@gmail.com>
2024-12-08 17:43:05 +0530

    changes

--- app/loader/page.tsx
+++ app/loader/page.tsx
@@ -1,21 +1,35 @@
 "use client"
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import {useLottie, useLottieInteractivity} from "lottie-react";
+import LottieFiles from '@/public/lottie/Animation - 1733656439178.json';
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'lottie-player': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-        src: string;
-        background: string;
-        speed: string;
-        style: React.CSSProperties;
-        autoplay: boolean;
-        direction: string;
-        mode: string;
-      };
-    }
-  }
+const style = {
+    width: 200,
+    height: 200,
+    background: 'transparent',
+}
+
+const options = {
+    animationData: LottieFiles,
+    loop: true,
+    autoplay: true,
+}
+
+const Lottie = () => {
+    const lottieObj = useLottie(options,style);
+    const Animation = useLottieInteractivity({
+        lottieObj,
+        actions: [
+            {
+                visibility: [0, 1],
+                type: "seek",
+                frames: [0, 100],
+            },
+        ],
+        mode: "scroll",
+    });
+    return <div>{Animation}</div>;;
 }
 
 const Loader = () => {
@@ -24,35 +38,16 @@ const Loader = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       router.push('/instruction'); 
-    }, 2000);
+    }, 10000);
 
     return () => clearTimeout(timer);
   }, [router]);
-
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://unpkg.com/@lottiefiles/lottie-player@2.0.8/dist/lottie-player.js';
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      // Clean up script if needed
-      document.body.removeChild(script);
-    };
-  }, []);
+  
 
   return (
     <div className="flex flex-col items-center justify-center bg-gray-900 text-white min-h-screen px-6 lg:px-16">
       <div className="flex justify-center">
-        <lottie-player
-          src="https://lottie.host/e6dce472-d28b-4f3d-9e8d-6105c068264d/FuXa0n3ZkY.json"
-          background="transparent"
-          speed="1"
-          style={{ width: '300px', height: '300px' }}
-          autoplay
-          direction="1"
-          mode="normal"
-        />
+        <Lottie />
       </div>
     </div>
   );
